a68214d87ff2a4f487e3b1c273acab6ffdaf2731
IgorKruchinin <igoryanbex@yandex.ru>
2025-08-22 15:34:31 +0300

    Add project files.

---
+++ Practice1-2025/WordAutomatisation.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Practice1_2025
+{
+    partial class WordAutomatisation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWorkName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTopic = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblDocumentType = new System.Windows.Forms.Label();
+            this.lblWorkType = new System.Windows.Forms.Label();
+            this.lblWorkNumber = new System.Windows.Forms.Label();
+            this.lblChackerName = new System.Windows.Forms.Label();
+            this.lblCheckerRegalies = new System.Windows.Forms.Label();
+            this.labellblUniversityName = new System.Windows.Forms.Label();
+            this.lblDepartament = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWorkName
+            // 
+            this.lblWorkName.AutoSize = true;
+            this.lblWorkName.Location = new System.Drawing.Point(18, 20);
+            this.lblWorkName.Name = "lblWorkName";
+            this.lblWorkName.Size = new System.Drawing.Size(127, 16);
+            this.lblWorkName.TabIndex = 0;
+            this.lblWorkName.Text = "Название работы:";
+            this.lblWorkName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(621, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblTopic
+            // 
+            this.lblTopic.AutoSize = true;
+            this.lblTopic.Location = new System.Drawing.Point(101, 49);
+            this.lblTopic.Name = "lblTopic";
+            this.lblTopic.Size = new System.Drawing.Size(44, 16);
+            this.lblTopic.TabIndex = 2;
+            this.lblTopic.Text = "Тема:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(618, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Location = new System.Drawing.Point(55, 87);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(90, 16);
+            this.lblSubject.TabIndex = 4;
+            this.lblSubject.Text = "Дисциплина:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(154, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(617, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // lblDocumentType
+            // 
+            this.lblDocumentType.AutoSize = true;
+            this.lblDocumentType.Location = new System.Drawing.Point(36, 120);
+            this.lblDocumentType.Name = "lblDocumentType";
+            this.lblDocumentType.Size = new System.Drawing.Size(109, 16);
+            this.lblDocumentType.TabIndex = 6;
+            this.lblDocumentType.Text = "Вид документа:";
+            // 
+            // lblWorkType
+            // 
+            this.lblWorkType.AutoSize = true;
+            this.lblWorkType.Location = new System.Drawing.Point(55, 150);
+            this.lblWorkType.Name = "lblWorkType";
+            this.lblWorkType.Size = new System.Drawing.Size(86, 16);
+            this.lblWorkType.TabIndex = 7;
+            this.lblWorkType.Text = "Вид работы:";
+            // 
+            // lblWorkNumber
+            // 
+            this.lblWorkNumber.AutoSize = true;
+            this.lblWorkNumber.Location = new System.Drawing.Point(37, 181);
+            this.lblWorkNumber.Name = "lblWorkNumber";
+            this.lblWorkNumber.Size = new System.Drawing.Size(104, 16);
+            this.lblWorkNumber.TabIndex = 8;
+            this.lblWorkNumber.Text = "Номер работы:";
+            // 
+            // lblChackerName
+            // 
+            this.lblChackerName.AutoSize = true;
+            this.lblChackerName.Location = new System.Drawing.Point(42, 232);
+            this.lblChackerName.Name = "lblChackerName";
+            this.lblChackerName.Size = new System.Drawing.Size(135, 16);
+            this.lblChackerName.TabIndex = 9;
+            this.lblChackerName.Text = "Имя проверяющего:";
+            // 
+            // lblCheckerRegalies
+            // 
+            this.lblCheckerRegalies.AutoSize = true;
+            this.lblCheckerRegalies.Location = new System.Drawing.Point(13, 261);
+            this.lblCheckerRegalies.Name = "lblCheckerRegalies";
+            this.lblCheckerRegalies.Size = new System.Drawing.Size(164, 16);
+            this.lblCheckerRegalies.TabIndex = 10;
+            this.lblCheckerRegalies.Text = "Регалии проверяющего:";
+            // 
+            // labellblUniversityName
+            // 
+            this.labellblUniversityName.AutoSize = true;
+            this.labellblUniversityName.Location = new System.Drawing.Point(66, 290);
+            this.labellblUniversityName.Name = "labellblUniversityName";
+            this.labellblUniversityName.Size = new System.Drawing.Size(111, 16);
+            this.labellblUniversityName.TabIndex = 11;
+            this.labellblUniversityName.Text = "Название вуза:";
+            // 
+            // lblDepartament
+            // 
+            this.lblDepartament.AutoSize = true;
+            this.lblDepartament.Location = new System.Drawing.Point(6, 348);
+            this.lblDepartament.Name = "lblDepartament";
+            this.lblDepartament.Size = new System.Drawing.Size(171, 16);
+            this.lblDepartament.TabIndex = 12;
+            this.lblDepartament.Text = "Наименование кафедры:";
+            this.lblDepartament.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(128, 392);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(49, 16);
+            this.lblCity.TabIndex = 13;
+            this.lblCity.Text = "Город:";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(144, 425);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(33, 16);
+            this.lblYear.TabIndex = 14;
+            this.lblYear.Text = "Год:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(185, 444);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(91, 24);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(328, 444);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(92, 23);
+            this.btnCreate.TabIndex = 16;
+            this.btnCreate.Text = "Создать";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // WordAutomatisation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 491);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblDepartament);
+            this.Controls.Add(this.labellblUniversityName);
+            this.Controls.Add(this.lblCheckerRegalies);
+            this.Controls.Add(this.lblChackerName);
+            this.Controls.Add(this.lblWorkNumber);
+            this.Controls.Add(this.lblWorkType);
+            this.Controls.Add(this.lblDocumentType);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblTopic);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblWorkName);
+            this.Name = "WordAutomatisation";
+            this.Text = "Выполнил: Кручинин И.Н.; Номер варианта: 6; Дата выполнения: 22.08.2025";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWorkName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTopic;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblDocumentType;
+        private System.Windows.Forms.Label lblWorkType;
+        private System.Windows.Forms.Label lblWorkNumber;
+        private System.Windows.Forms.Label lblChackerName;
+        private System.Windows.Forms.Label lblCheckerRegalies;
+        private System.Windows.Forms.Label labellblUniversityName;
+        private System.Windows.Forms.Label lblDepartament;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
